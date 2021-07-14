60ce7c0e7ad7409b17c0b002d9e1b09b07220cf2
Pietro Lauria <ptr.lauria@gmail.com>
2021-07-14 11:26:40 +0200

    y

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,128 +1,100 @@
 'use strict';
 
-var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var Counter = function (_React$Component) {
+  _inherits(Counter, _React$Component);
 
-// We use classes to reuse code
-// Class = blueprint. Then we make multiples instances of that class for the individual items
-// The structure is the same (same features), but each one can have some unique features
+  function Counter(props) {
+    _classCallCheck(this, Counter);
 
-var Person = function () {
-  function Person() {
-    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
-    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
 
-    _classCallCheck(this, Person);
-
-    this.name = name;
-    this.age = age;
-  }
-
-  _createClass(Person, [{
-    key: 'getGreeting',
-    value: function getGreeting() {
-      // return 'Hi. I am ' + this.name + '!';
-      return 'Hi. I am ' + this.name + '!';
-    }
-  }, {
-    key: 'getDescription',
-    value: function getDescription() {
-      return this.name + ' is ' + this.age + ' year(s) old.';
-    }
-  }]);
-
-  return Person;
-}();
-
-// create a subclass of person
-
-
-var Student = function (_Person) {
-  _inherits(Student, _Person);
-
-  function Student(name, age, major) {
-    _classCallCheck(this, Student);
-
-    // calling the parent constructor function
-    var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
-
-    _this.major = major;
+    _this.handleAddOne = _this.handleAddOne.bind(_this);
+    _this.handleMinusOne = _this.handleMinusOne.bind(_this);
+    _this.handleReset = _this.handleReset.bind(_this);
     return _this;
   }
 
-  _createClass(Student, [{
-    key: 'hasMajor',
-    value: function hasMajor() {
-      return !!this.major; //  ***
-    }
-    // overriding the getDescription() of the parent class
-
-  }, {
-    key: 'getDescription',
-    value: function getDescription() {
-      var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
-
-      if (this.hasMajor()) {
-        description += ' His major is ' + this.major + '.';
-      }
-
-      return description;
-    }
-  }]);
-
-  return Student;
-}(Person);
-
-// create another subclass of person
-
-
-var Traveler = function (_Person2) {
-  _inherits(Traveler, _Person2);
-
-  function Traveler(name, age, homeLocation) {
-    _classCallCheck(this, Traveler);
-
-    var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age));
-
-    _this2.homeLocation = homeLocation;
-    return _this2;
-  }
-
-  _createClass(Traveler, [{
-    key: 'getGreeting',
-    value: function getGreeting() {
-      var greeting = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
-
-      if (this.homeLocation) {
-        greeting += 'I am visiting from ' + this.homeLocation;
-      }
-      return greeting;
+  _createClass(Counter, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          'Count:'
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.handleAddOne },
+          '+1'
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.handleMinusOne },
+          '-1'
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.handleReset },
+          'Reset'
+        )
+      );
     }
   }]);
 
-  return Traveler;
-}(Person);
+  return Counter;
+}(React.Component);
 
-// make a new instance of the class Person
+var handleAddOne = function handleAddOne() {
+  console.log('handleAddOne');
+};
+var handleMinusOne = function handleMinusOne() {
+  console.log('handleMinusOne');
+};
+var handleReset = function handleReset() {
+  console.log('handleReset');
+};
 
+ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
 
-var me = new Traveler('Pietro Lauria', 43, 'Coscitello');
-console.log(me.getGreeting());
+// let count = 0;
 
-var other = new Traveler();
-console.log(other.getGreeting());
+// const addOne = () => {
+//   count++;
+//   renderCounterApp();
+// };
+// const minusOne = () => {
+//   count--;
+//   renderCounterApp();
+// };
+// const reset = () => {
+//   count = 0;
+//   renderCounterApp();
+// };
 
-// *** !!this.major => could be either a string (thruty value) or undefined (falsy value), so we convert the truty(falsy) value to true(false)
+// const appRoot = document.getElementById('app');
 
-/* '' falsy value
-   !'' convert the falsy  to  boolean true
-   !!'' convert the falsy value and convert to false 
-   !!'pietro' convert a truty value to true
-   */
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>reset</button>
+//     </div>
+//   );
+
+//   ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
